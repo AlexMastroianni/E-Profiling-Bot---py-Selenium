b30fca92678be581c4d79c8049b9165cc0d804d2
AlexMastroianni <alex.m.mastroianni@gmail.com>
2022-09-13 15:46:22 +1000

    add workplace fillout and first card

--- index.py
+++ index.py
@@ -33,7 +33,6 @@ firstCard = url.find_element("xpath", "/html/body/div[1]/div[3]/div[2]/table/tbo
 firstCard.click()
 
 
-
 time.sleep(2)
 supervisorDropdown = url.find_element("xpath", "//tbody/tr/td/div[1]/select[2]")
 supervisorDropdown.click()
@@ -46,4 +45,77 @@ supervisorSelect.click()
 time.sleep(0.5)
 expand = url.find_element("xpath", "/html/body/div[1]/div[3]/div[2]/form/div[1]/p/span[1]")
 expand.click()
+
+
+
+time.sleep(1)
+# Timer 1
+timer1Dropdown = url.find_element("xpath", "/html/body/div[1]/div[3]/div[2]/form/div[1]/table[2]/tbody/tr[2]/td/table[1]/tbody/tr/td/div[3]/table/tbody/tr[2]/td/div/table/tbody/tr[1]/td[2]/select")
+timer1Dropdown.click()
+
+timer1Select = url.find_element("xpath", "/html/body/div[1]/div[3]/div[2]/form/div[1]/table[2]/tbody/tr[2]/td/table[1]/tbody/tr/td/div[3]/table/tbody/tr[2]/td/div/table/tbody/tr[1]/td[2]/select/option[33]")
+timer1Select.click()
+
+
+# Card Fillout
+time.sleep(2)
+selectbutton1 = url.find_element("xpath", "/html/body/div[1]/div[3]/div[2]/form/div[1]/table[2]/tbody/tr[2]/td/table[1]/tbody/tr/td/div[3]/table/tbody/tr[2]/td/div/table/tbody/tr[2]/td[2]/table/tbody/tr/td[2]/input")
+selectbutton1.click()
+
+selectbutton2 = url.find_element("xpath", "/html/body/div[1]/div[3]/div[2]/form/div[1]/table[2]/tbody/tr[2]/td/table[1]/tbody/tr/td/div[3]/table/tbody/tr[2]/td/div/table/tbody/tr[3]/td[2]/table/tbody/tr/td[2]/input")
+selectbutton2.click()
+
+selectbutton3 = url.find_element("xpath", "/html/body/div[1]/div[3]/div[2]/form/div[1]/table[2]/tbody/tr[2]/td/table[1]/tbody/tr/td/div[3]/table/tbody/tr[2]/td/div/table/tbody/tr[4]/td[2]/table/tbody/tr/td[2]/input")
+selectbutton3.click()
+
+selectbutton4 = url.find_element("xpath", "/html/body/div[1]/div[3]/div[2]/form/div[1]/table[2]/tbody/tr[2]/td/table[1]/tbody/tr/td/div[3]/table/tbody/tr[2]/td/div/table/tbody/tr[5]/td[2]/table/tbody/tr[1]/td[1]/input")
+selectbutton4.click()
+
+selectbutton5 = url.find_element("xpath", "/html/body/div[1]/div[3]/div[2]/form/div[1]/table[2]/tbody/tr[2]/td/table[1]/tbody/tr/td/div[3]/table/tbody/tr[2]/td/div/table/tbody/tr[5]/td[2]/table/tbody/tr[3]/td[1]/input")
+selectbutton5.click()
+
+selectbutton6 = url.find_element("xpath", "/html/body/div[1]/div[3]/div[2]/form/div[1]/table[2]/tbody/tr[2]/td/table[1]/tbody/tr/td/div[3]/table/tbody/tr[2]/td/div/table/tbody/tr[5]/td[2]/table/tbody/tr[4]/td[1]/input")
+selectbutton6.click()
+
+selectbutton7 = url.find_element("xpath", "/html/body/div[1]/div[3]/div[2]/form/div[1]/table[2]/tbody/tr[2]/td/table[1]/tbody/tr/td/div[3]/table/tbody/tr[2]/td/div/table/tbody/tr[5]/td[2]/table/tbody/tr[11]/td[1]/input")
+selectbutton7.click()
+
+selectbutton8 = url.find_element("xpath", "/html/body/div[1]/div[3]/div[2]/form/div[1]/table[2]/tbody/tr[2]/td/table[1]/tbody/tr/td/div[3]/table/tbody/tr[2]/td/div/table/tbody/tr[5]/td[2]/table/tbody/tr[12]/td[1]/input")
+selectbutton8.click()
+
+selectbutton9 = url.find_element("xpath", "/html/body/div[1]/div[3]/div[2]/form/div[1]/table[2]/tbody/tr[2]/td/table[1]/tbody/tr/td/div[3]/table/tbody/tr[2]/td/div/table/tbody/tr[5]/td[2]/table/tbody/tr[11]/td[2]/input")
+selectbutton9.click()
+
+time.sleep(1)
+# Work Practices
+button1 = url.find_element("xpath", "/html/body/div[1]/div[3]/div[2]/form/div[1]/table[4]/tbody/tr[1]/td/input")
+button1.click()
+
+button2 = url.find_element("xpath", "/html/body/div[1]/div[3]/div[2]/form/div[1]/table[4]/tbody/tr[2]/td/input")
+button2.click()
+
+button3 = url.find_element("xpath", "/html/body/div[1]/div[3]/div[2]/form/div[1]/table[4]/tbody/tr[3]/td/input")
+button3.click()
+
+button4 = url.find_element("xpath", "/html/body/div[1]/div[3]/div[2]/form/div[1]/table[4]/tbody/tr[4]/td/input")
+button4.click()
+
+button5 = url.find_element("xpath", "/html/body/div[1]/div[3]/div[2]/form/div[1]/table[4]/tbody/tr[5]/td/input")
+button5.click()
+
+button6 = url.find_element("xpath", "/html/body/div[1]/div[3]/div[2]/form/div[1]/table[4]/tbody/tr[6]/td/input")
+button6.click()
+
+button7 = url.find_element("xpath", "/html/body/div[1]/div[3]/div[2]/form/div[1]/table[4]/tbody/tr[7]/td/input")
+button7.click()
+
+
+
+
+
+
+
+
+
+
 
