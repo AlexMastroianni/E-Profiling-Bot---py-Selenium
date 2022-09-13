9632f0bb4e0329b6ae057b44d52fa9ae8e80f20a
AlexMastroianni <alex.m.mastroianni@gmail.com>
2022-09-13 15:03:58 +1000

    updated

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ import time
 url = webdriver.Chrome()
 url.get("https://elogbook.eprofiling.com.au/index.cfm?event=editCard&cardID=&qualificationID=E5C5B039-0A48-4103-869E-A7D2CB4417EC&yearNo=2020&weekNo=23&apprenticeID=389D8235-466A-6537-6E13-A4A219C87018")
 
-time.sleep(5)
+time.sleep(2)
 
 username = "al@dvineelectrics.com.au"
 password = "password123"
@@ -19,7 +19,7 @@ passwordInput = url.find_element("xpath", "/html/body/div[1]/div[3]/div[1]/table
 passwordInput.send_keys(password)
 
 
-time.sleep(2)
+time.sleep(0.5)
 loginSubmit = url.find_element("xpath", "/html/body/div[1]/div[3]/div[1]/table/tbody/tr[2]/td[1]/form/div/input")
 loginSubmit.click()
 
@@ -28,16 +28,22 @@ time.sleep(2)
 cards = url.find_element("xpath", "/html/body/div[1]/div[2]/ul/li[2]/a")
 cards.click()
 
-time.sleep(2)
+time.sleep(0.5)
 firstCard = url.find_element("xpath", "/html/body/div[1]/div[3]/div[2]/table/tbody[2]/tr[1]/td[2]/a")
 firstCard.click()
 
 
+
 time.sleep(2)
+supervisorDropdown = url.find_element("xpath", "//tbody/tr/td/div[1]/select[2]")
+supervisorDropdown.click()
+
+time.sleep(0.5)
+supervisorSelect = url.find_element("xpath","//tbody//div[1]//select[2]//option[2]")
+supervisorSelect.click()
+
+
+time.sleep(0.5)
 expand = url.find_element("xpath", "/html/body/div[1]/div[3]/div[2]/form/div[1]/p/span[1]")
 expand.click()
-
-
-
-# Fault finding
 
